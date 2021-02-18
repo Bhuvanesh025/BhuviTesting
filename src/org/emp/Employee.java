6a8bd2e435694912c895b8b21e19ea37a05de079
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-02-18 22:32:42 +0530

    Person1 added his code

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -4,29 +4,22 @@ public class Employee {
 	public void empId() {
 		System.out.println("Emp ID:001");
 	}
-	public void empName() {
-		System.out.println("Emp Name: Bhuvanesh.E");
-	}
-	public void empDob() {
-		System.out.println("Emp Dob: 8th March");
-	}
-	public void empPhone() {
-		System.out.println("Emp Phone: 123456");
-	}
-	public void empEmail() {
-		System.out.println("Emp Email: abc@gmail.com");
-	}
-	public void empAddress() {
-		System.out.println("Emp Address: xyz,Indra nagar,Cbe-639586");
-	}
+		public void phoneInfo(String phname,int phnprice) {
+			
+			System.out.println("Phone name is:"+phname+"\t"+"Phone price is:"+phnprice);
+			
+		}
+		
+	public void phoneInfo(int camerapixel,String imeino ) {
+			
+			System.out.println("Phone camera Pixel is:"+camerapixel+"\n"+"Phone price is:"+imeino);
+			
+		}
+	
+	
 	public static void main(String[] args) {
 		Employee c =new Employee();
-		c.empId();
-		c.empName();
-		c.empDob();
-		c.empPhone();
-		c.empEmail();
-		c.empAddress();
+		
 	}
 	
 	
